28c96a4565695a8fbab627901e598b9665495060
wentywenty <2321901849@qq.com>
2026-05-19 11:44:59 +0800

    update lic

--- start_robot.sh
+++ start_robot.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Copyright (C) 2025-2026 Luo1imasi
+# Copyright (C) 2025-2026 wentywenty
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # 颜色定义，用于美化输出
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
